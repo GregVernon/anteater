7d6c1160f900ecd80331a99a50a5c3321c9dc286
Derek Gaston <derek.gaston@inl.gov>
2018-07-30 17:00:33 -0600

    Add Cubit script for hollow_cyl_hole mesh

---
+++ problems/geom/hollow_cyl_hole.py
@@ -0,0 +1,34 @@
+#!python
+cubit.cmd('reset')
+cubit.cmd('create cylinder height 30 radius 5')
+cubit.cmd('create cylinder height 30 radius 4')
+cubit.cmd('subtract volume 2 from volume 1')
+cubit.cmd('compress ids')
+cubit.cmd('cylinder radius 2.5 height 10')
+cubit.cmd('rotate volume 2 angle 90 about y include_merged')
+cubit.cmd('subtract volume 2 from volume 1')
+cubit.cmd('compress ids')
+cubit.cmd('webcut volume all with plane xplane offset 0')
+cubit.cmd('webcut volume all with plane zplane offset 0')
+cubit.cmd('webcut volume all with plane zplane offset 5')
+cubit.cmd('webcut volume all with plane zplane offset -5')
+cubit.cmd('webcut volume all with plane zplane offset 0 rotate 90 about x')
+cubit.cmd('imprint all')
+cubit.cmd('merge all')
+cubit.cmd('compress ids')
+cubit.cmd('volume all size 0.5')
+cubit.cmd('mesh volume all')
+
+cubit.cmd('block 1 add volume all')
+
+cubit.cmd('Sideset 1 add surface 21 14 23 30')
+cubit.cmd('sideset 1 name "left"')
+
+cubit.cmd('Sideset 2 add surface 64 71 76 69')
+cubit.cmd('sideset 2 name "right"')
+
+cubit.cmd('export genesis "~/projects/anteater/problems/geom/hollow_cyl_hole_r0.e" overwrite')
+
+#for i in xrange(0,2):
+#    cubit.cmd('refine volume all numsplit 1 bias 1 depth 1 smooth')
+#    cubit.cmd('export genesis "~/projects/anteater/problems/geom/hollow_cyl_hole_r' + str(i + 1) + '.e" overwrite')
