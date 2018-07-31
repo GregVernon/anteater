b04c14ee6c8a32d48bf265ba1be2c8b8099a129b
Greg Vernon <greg.j.vernon@gmail.com>
2018-07-31 12:16:40 -0600

    Move meshing and export into loop

--- problems/geom/hollow_cyl_hole.py
+++ problems/geom/hollow_cyl_hole.py
@@ -1,4 +1,7 @@
 #!python
+#!python
+#!python
+#!python
 cubit.cmd('reset')
 cubit.cmd('create cylinder height 30 radius 5')
 cubit.cmd('create cylinder height 30 radius 4')
@@ -16,8 +19,6 @@ cubit.cmd('webcut volume all with plane zplane offset 0 rotate 90 about x')
 cubit.cmd('imprint all')
 cubit.cmd('merge all')
 cubit.cmd('compress ids')
-cubit.cmd('volume all size 0.5')
-cubit.cmd('mesh volume all')
 
 cubit.cmd('block 1 add volume all')
 
@@ -27,8 +28,14 @@ cubit.cmd('sideset 1 name "left"')
 cubit.cmd('Sideset 2 add surface 64 71 76 69')
 cubit.cmd('sideset 2 name "right"')
 
-cubit.cmd('export genesis "~/projects/anteater/problems/geom/hollow_cyl_hole_r0.e" overwrite')
+for i in xrange(0,2):
+    if i == 0:
+        cubit.cmd('volume all size 0.5')
+        cubit.cmd('mesh volume all')
+    else:
+        cubit.cmd('delete mesh')
+        cubit.cmd('refine volume 9 13 numsplit 1 bias 1 depth 1 smooth')
+        cubit.cmd('mesh volume all')
+    cubit.cmd('export genesis "~/projects/anteater/problems/geom/hollow_cyl_hole_r' + str(i) + '.e" overwrite')
 
-#for i in xrange(0,2):
-#    cubit.cmd('refine volume all numsplit 1 bias 1 depth 1 smooth')
-#    cubit.cmd('export genesis "~/projects/anteater/problems/geom/hollow_cyl_hole_r' + str(i + 1) + '.e" overwrite')
+
