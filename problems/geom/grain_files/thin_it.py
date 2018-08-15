05033686636165ddfc6c4bad91f435d61ef1e964
Derek Gaston <derek.gaston@inl.gov>
2018-08-15 09:05:26 -0600

    Add coarsened grain positions file

---
+++ problems/geom/grain_files/thin_it.py
@@ -0,0 +1,11 @@
+import random
+
+keep_percent = 10.
+
+position_file_lines = open("maximal_sample_bf-4802.txt").readlines()
+
+new_position_file = open("coarsened_positions.txt", 'w')
+
+kept_lines = random.sample(position_file_lines, 400)
+
+new_position_file.writelines(kept_lines)
